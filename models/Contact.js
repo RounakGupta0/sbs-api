b6a0aacb7f04eee5bf3071f4ab4d7c7f66570329
RounakGupta0 <rounakgupta620@gmail.com>
2026-04-08 08:36:26 +0530

    first changes

---
+++ models/Contact.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const contactSchema = new mongoose.Schema({
+    fullName:{type:String,required:true},
+    phone:{type:String,required:true},
+    email:{type:String,required:true},
+    address:{type:String,required:true},
+    gender:{type:String,required:true}
+})
+
+module.exports = mongoose.model('contact',contactSchema)
