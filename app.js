ea48f421dfcfd6e94ff36116cc7f55347432437e
RounakGupta0 <rounakgupta620@gmail.com>
2026-04-09 08:20:58 +0530

    secured mongodb with .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const userRoute = require('./Routes/user')
@@ -6,7 +7,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 
-// mongoose.connect('mongodb+srv://rounak:rounak1234@sbs.4aiqmhs.mongodb.net/?appName=SBS')
+// mongoose.connect()
 // .then(()=>{
 //     console.log("connected with database")
 // })
@@ -17,7 +18,7 @@ const bodyParser = require('body-parser')
 
 const connectWithDatabase = async()=>{
     try{
-        await mongoose.connect('mongodb+srv://rounak:rounak1234@sbs.4aiqmhs.mongodb.net/?appName=SBS')
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("connected with database")
     }
     catch(err){
